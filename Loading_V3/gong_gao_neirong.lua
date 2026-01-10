ff69dea5b7af4681765e07eac1d8fbb4c54e1bc9
wwww666999 <206301094+wwww666999@users.noreply.github.com>
2026-01-10 20:48:32 +0800

    Update gong_gao_neirong.lua

--- Loading_V3/gong_gao_neirong.lua
+++ Loading_V3/gong_gao_neirong.lua
@@ -1,4 +1,5 @@
-local b = 1
-gong_gao_Build("wwww666999","创始人","2025-1-10-19:24",b)
+--local b = 1
+gong_gao_Build("wwww666999","创始人","2025-1-10-19:24",1)
 local Text1 = Instance.new("TextLable")
-Text1.Parent = Main_Zhu_1_Home_gonggao_gaoshi.b
+Text1.Parent = Main_Zhu_1_Home_gonggao_gaoshi.1
+
